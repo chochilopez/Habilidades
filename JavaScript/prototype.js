0322c39241e952ebbd904d18bb32e8a72ce65e6e
A A <a@a.com>
2023-06-29 17:56:49 -0300

    aa

--- JavaScript/prototype.js
+++ JavaScript/prototype.js
@@ -7,5 +7,24 @@ Tests:
 
 
 */
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
 
-console.log();
+function golfScore(par, strokes) {
+  if (strokes === 1)
+    return names[0];
+  else if ((strokes - par) <= -2)
+    return  names[1];
+  else if ((strokes - par) <= -1)
+    return  names[2];
+  else if (strokes === par) 
+    return  names[3];
+  else if ((strokes - par) >= 3)
+    return  names[6];
+  else if ((strokes - par) >= 2)
+    return  names[5];
+  else
+    return  names[4];
+
+}
+
+golfScore(5, 4);
