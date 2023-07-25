14a655c96b8ebe2605698b255b588f53447df7c4
Gastón López <lopezwgaston@gmail.com>
2023-07-25 11:36:38 -0300

    ES6 from 3 to 10

---
+++ JavaScript/ES6/5-Write arrow functions with parameters.js
@@ -0,0 +1,37 @@
+/*
+Explanation:
+Just like a regular function, you can pass arguments into an arrow function.
+
+const doubler = (item) => item * 2;
+doubler(4);
+doubler(4) would return the value 8.
+
+If an arrow function has a single parameter, the parentheses enclosing the parameter may be omitted.
+
+const doubler = item => item * 2;
+It is possible to pass more than one argument into an arrow function.
+
+const multiplier = (item, multi) => item * multi;
+multiplier(4, 2);
+multiplier(4, 2) would return the value 8.
+
+Exercise:
+Rewrite the myConcat function which appends contents of arr2 to arr1 so that the function uses arrow function syntax.
+
+Tests:
+Waiting:You should replace the var keyword.
+Waiting:myConcat should be a constant variable (by using const).
+Waiting:myConcat should be an arrow function with two parameters
+Waiting:myConcat() should return [1, 2, 3, 4, 5].
+Waiting:The function keyword should not be used.
+
+var myConcat = function(arr1, arr2) {
+  return arr1.concat(arr2);
+};
+
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+*/
+const myConcat = (arr1, arr2) => arr1.concat(arr2)
+
+console.log(myConcat([1, 2], [3, 4, 5]));
